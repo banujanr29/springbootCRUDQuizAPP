fb14b550ac18b79abd64cc5546a0d4e25f98bc08
Banujan Ravishankar <banujanr29@gmail.com>
2025-02-20 17:06:51 +0530

    first commit

---
+++ src/main/java/com/banu/quizapp/SbQuizAppApplication.java
@@ -0,0 +1,13 @@
+package com.banu.quizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbQuizAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbQuizAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/banu/quizapp/SbQuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banu.quizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbQuizAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
