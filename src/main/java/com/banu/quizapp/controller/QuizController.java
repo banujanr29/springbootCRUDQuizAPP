44b3195db18edba379b914ed69677fa51a3cd153
Banujan Ravishankar <banujanr29@gmail.com>
2025-02-24 13:01:54 +0530

    quiz-fetching-done

--- src/main/java/com/banu/quizapp/controller/QuizController.java
+++ src/main/java/com/banu/quizapp/controller/QuizController.java
@@ -1,9 +1,13 @@
 package com.banu.quizapp.controller;
 
+import com.banu.quizapp.model.QuestionWrapper;
+import com.banu.quizapp.model.Quiz;
 import com.banu.quizapp.service.QuizService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/quiz")
@@ -15,8 +19,13 @@ public class QuizController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<String> createQuiz(@RequestParam String difficultylevel, @RequestParam int numQ, @RequestParam String title) {
-        return createQuiz(difficultylevel, numQ, title);
+    public ResponseEntity<String> createQuiz(@RequestParam String difficultylevel, @RequestParam Integer numQ, @RequestParam String title) {
+        return quizService.createQuiz(difficultylevel, numQ, title);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id) {
+        return quizService.getQuizQuestion(id);
     }
 
 }
